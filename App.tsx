ab9530e789993a3e730ad9cd9b2c4b22b95c85d7
rodrex25 <gabresh25gmail.com>
2024-02-26 19:00:20 -0600

    set app
    Co-authored-by: Customi24 <Customi24@users.noreply.github.com>

--- App.tsx
+++ App.tsx
@@ -1,118 +1,113 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState, useEffect} from 'react';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, TouchableOpacity, Image} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const [dog, setDog] = useState('');
+  const [dogData, setDogData] = useState(null);
+  const [error, setError] = useState(null);
+  
+  const fetchDogApp = async () => {
+    
+    const x_api_key = 'live_362rpNfPUVZkdZIgHTzK2qaJUMkuMVPIehAsswifE6Qt90z4YqRCADyd10eZ91ll'
+    
+    try{
+      const res = await fetch('https://dog.ceo/api/breeds/image/random')
+      const data = await res.json();
+      setDogData(data);
+      setError(null);
+    }
+    catch (e){
+      console.error(e)
+      setError('Error fetching Dog data');
+    }
   };
 
+  useEffect( () => {
+    dog ? fetchDogApp : setError;
+  }, [dog]);
+
+  console.log(dogData);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+
+      <View>
+        <Text style={styles.title}>Random Dog App</Text>
+        
+        {error && (<Text>{error}</Text>)}
+      {dogData && (
+        <View style={styles.clima}>
+          <>
+          <Image
+            style={styles.imgdog}
+            source={{uri: dogData.message}}/>
+          
+          </>
+         
         </View>
-      </ScrollView>
+      )}
+        
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={fetchDogApp}>
+          <Text>RANDOM</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={fetchDogApp}>
+          <Text>SELECT</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={fetchDogApp}>
+          <Text>DELETE</Text>
+        </TouchableOpacity>
+
+      </View>
+      
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    marginBottom:20,
+    alignContent: 'center',
+    backgroundColor: '#99C3D1',
+    height:'100%',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  button: {
+    backgroundColor: '#0BD5C4',
+    width: 250,
+    height: 35,
+    borderRadius: 5,
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: '15%',
   },
-  highlight: {
-    fontWeight: '700',
+  imgdog:{
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent:'center',
+    width: 300,
+    height: 300,
+
+  },
+  clima:{
+    
+    marginTop: 100,
+    alignItems: 'center',
+    justifyContent:'center',
+    elevation: 5,
+    borderRadius: 10, 
+    marginLeft: 6,
+    
   },
 });
-
-export default App;
